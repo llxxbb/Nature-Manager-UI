0ebf33d383877cd2e08426e117bb26d81474f8b8
llxxbb <llxxbb@yeah.net>
2020-12-13 15:02:15 +0800

    a workable tree

--- src/components/ShowArea.vue
+++ src/components/ShowArea.vue
@@ -1,27 +1,10 @@
 <template>
-  <svg
-    id="showArea"
-    ref="showArea"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 800 700"
-    preserveAspectRatio="none"
-  />
+  <svg id="showArea" ref="showArea" xmlns="http://www.w3.org/2000/svg" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import * as d3 from "d3";
-import { HierarchyNode } from "d3";
-
-class Position {
-  x: number = 0;
-  y: number = 0;
-}
-
-class Node{
-  name: String = ""
-  children: Node[] = []
-}
+import { D3Tree, TreePara, Node } from "./d3tree";
 
 @Options({
   data() {
@@ -30,81 +13,35 @@ class Node{
     };
   },
   computed: {
-    center: function () {
+    center() {
       let area = this.$refs.showArea!;
-      let x = area.clientWidth / 2;
-      let y = area.clientHeight / 2;
-      return [x, y];
+      return [area.clientWidth, area.clientHeight];
     },
   },
   methods: {},
   mounted() {
-    let data = {
-      name:"parent",
-      children:[
+    let data: Node = {
+      name: "L1",
+      children: [
         {
-          name: "c1",
+          name: "L2-1",
+          children: [{ name: "L3", children: [] }],
         },
         {
-          name: "c2"
-        }
-      ]
-    }
-    let x0 = Infinity;
-    let x1 = -x0;
-    let hierarchy = d3.hierarchy(data)
-    let root = d3.tree()(hierarchy)
-    root.x = 300
-    root.y = 500
-    // const root = tree(data);
-
-    this.svg = d3.select("#showArea");
-    const g = this.svg
-      .append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("transform", `translate(${10 / 3},${100 - x0})`);
-
-    const link = g
-      .append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
-      .selectAll("path")
-      .data(root.links())
-      .join("path")
-      .attr(
-        "d",
-        d3
-          .linkHorizontal()
-          // .x((d) => d.y)
-          // .y((d) => d.x)
-      );
-
-    const node = g
-      .append("g")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-width", 3)
-      .selectAll("g")
-      .data(root.descendants())
-      .join("g")
-      .attr("transform", (d:Position) => `translate(${d.y},${d.x})`);
-
-    node
-      .append("circle")
-      .attr("fill", (d:Node) => (d.children ? "#555" : "#999"))
-      .attr("r", 2.5);
-
-    node
-      .append("text")
-      .attr("dy", "0.31em")
-      .attr("x", (d:Node) => (d.children ? -6 : 6))
-      .attr("text-anchor", (d:Node) => (d.children ? "end" : "start"))
-      .text((d:{data:Node}) => d.data.name)
-      .clone(true)
-      .lower()
-      .attr("stroke", "white");
+          name: "L2-2",
+          children: [],
+        },
+      ],
+    };
+    let para: TreePara = {
+      target: "#showArea",
+      size: {
+        width: this.center[0],
+        height: this.center[1],
+      },
+      data: data,
+    };
+    new D3Tree().show(para);
   },
 })
 export default class ShowArea extends Vue {}
---
+++ src/components/d3tree.ts
@@ -0,0 +1,95 @@
+import * as d3 from "d3";
+import { HierarchyPointNode } from "d3";
+
+export class Position {
+    x: number = 0;
+    y: number = 0;
+}
+
+export class Node {
+    name: string = "";
+    children: Node[] = [];
+}
+
+export class SvgSize {
+    width: number = 0;
+    height: number = 0;
+}
+export class TreePara {
+    target: string = ""
+    size: SvgSize = {} as any
+    data: Node = {} as any
+}
+export class D3Tree {
+    show(para: TreePara) {
+
+        let svg = d3.select(para.target);
+        // add viewBox for pan and zoom
+        svg.attr("viewBox", `0, 0, 1, 1`);
+
+        let nodes = appendProperty(para);
+
+        // draw line first! otherwise you will see the line goes into the circle
+        drawLinks(svg, nodes);
+        drawNodes(svg, nodes);
+
+    }
+}
+
+
+function appendProperty(para: TreePara) {
+    // append depth, height, children, parent properties to datum
+    let hierarchy = d3.hierarchy(para.data);
+    // append x, y properties to datum
+    let nodes = d3.tree()(hierarchy);
+    // get max depth
+    let maxDepth = 0;
+    nodes.each(d => { if (d.depth > maxDepth) maxDepth = d.depth })
+    return nodes
+    // .attr("transform", `translate(${root.y},${root.x})`);
+}
+
+function drawLinks(svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>, nodes: d3.HierarchyPointNode<unknown>) {
+    let linkFn = d3.linkHorizontal<any, any>()
+        .x(d => d.y)
+        .y(d => d.x);
+
+    const link = svg
+        .append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 0.005)
+        .selectAll("path")
+        .data(nodes.links())
+        .join("path")
+        .attr("d", linkFn);
+}
+function drawNodes(svg: d3.Selection<d3.BaseType, unknown, HTMLElement, any>, nodes: d3.HierarchyPointNode<unknown>) {
+    // set font property to node
+    let g = svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 0.04)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 0.006);
+
+    const node = g.selectAll("g")
+        .data(nodes.descendants())
+        .join("g")
+        .attr("transform", (d: Position) => `translate(${d.y},${d.x})`);
+
+    node.append("circle")
+        .attr("fill", (d) => (d.children ? "#048000" : "#07cc00"))
+        .attr("r", 0.03);
+
+    node.append("text")
+        .attr("y", "0.38em")
+        .attr("x", (d) => (d.children ? "-0.04" : "0.04"))
+        .attr("text-anchor", (d) => (d.children ? "end" : "start"))
+        .text((d) => ((d.data) as Node).name)
+        .clone(true)
+        .lower()
+        .attr("stroke", "white");
+
+}
+
