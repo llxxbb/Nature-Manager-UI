910d38c23ce40f591fd3db51638e7fc56434b594
lixuebin <lixuebin@cdeledu.com>
2021-01-24 14:20:16 +0800

    instance query menu

--- src/components/MetaContextMenu.vue
+++ src/components/MetaContextMenu.vue
@@ -5,27 +5,40 @@
     :style="{ top: para.top + 'px', left: para.left + 'px' }"
   >
     <ul class="list-group">
-      <li class="list-group-item item list-group-item-action">
+      <li
+        v-show="para.meta.canQueryInstance()"
+        class="list-group-item item list-group-item-action"
+      >
         <img src="../assets/locate.svg" />
         query instance
         <input v-model="instanceId" @keyup.enter="query" />
       </li>
-      <li class="list-group-item item list-group-item-action" @click="list">
+      <li
+        v-show="para.meta.canQueryInstance()"
+        class="list-group-item item list-group-item-action"
+        @click="list"
+      >
         <img src="../assets/list.svg" />
         query recent instances
       </li>
-      <li class="list-group-item item list-group-item-action" @click="editNode">
+      <li
+        v-show="false"
+        class="list-group-item item list-group-item-action"
+        @click="editNode"
+      >
         <img src="../assets/node-edit.svg" />
         edit node
       </li>
-      <li class="list-group-item item list-group-item-action">
+      <li v-show="false" class="list-group-item item list-group-item-action">
         <img src="../assets/node-plus.svg" />
         add child node
         <input v-model="metaName" @keyup.enter="addNode" />
       </li>
       <li
+        v-show="false"
         class="list-group-item item list-group-item-action"
-        @click="deleteNode">
+        @click="deleteNode"
+      >
         <img src="../assets/node-minus.svg" />
         delete node
       </li>
@@ -34,12 +47,13 @@
 </template>
 
 <script lang="ts">
+import { Meta } from "@/domain";
 import { Options, Vue } from "vue-class-component";
 
 export class CMPara {
   left = 0;
   top = 0;
-  node?: Node;
+  meta: Meta = new Meta();
 }
 
 @Options({
@@ -59,24 +73,24 @@ export class CMPara {
     add() {},
     query(e: KeyboardEvent) {
       this.$emit("hide");
-      this.$emit("instance", { id: this.instanceId, meta: this.para.node });
+      this.$emit("instance", { id: this.instanceId, meta: this.para.meta });
       this.instanceId = "";
     },
     list() {
       this.$emit("hide");
-      this.$emit("list", this.para.node);
+      this.$emit("list", this.para.meta);
     },
     editNode() {
       this.$emit("hide");
-      this.$emit("editNode", this.para.node);
+      this.$emit("editNode", this.para.meta);
     },
     deleteNode() {
       this.$emit("hide");
-      this.$emit("deleteNode", this.para.node);
+      this.$emit("deleteNode", this.para.meta);
     },
     addNode() {
       this.$emit("hide");
-      this.$emit("addNode", { name: this.metaName, parent: this.para.node });
+      this.$emit("addNode", { name: this.metaName, parent: this.para.meta });
       this.metaName = "";
     },
   },
